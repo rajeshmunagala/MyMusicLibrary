f9206e9e63ae4856d203df80f4499b65a72985d0
Rajesh Munagala <43266655+rajeshmunagala@users.noreply.github.com>
2019-03-23 11:35:38 +0530

    De_version_0.1
    This is the version in the development stage.
    Added features are
    1.Create albums
    2.Displaying albums
    3.Displaying album details
    4.Update album details
    and  so on....

---
+++ music/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'music'
+urlpatterns = [
+    # /music/
+    path('', views.IndexView.as_view(), name='index'),
+    path('register', views.UserFormView.as_view(), name='register'),
+    # /music/<album_id>/
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # /music/album/add
+    re_path(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+# /music/album/2
+    re_path(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+# /music/album/2/delete
+    re_path(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
+    # / music / < album_id >/favorite/
+#    re_path(r'^(?P<album_id>[0-9]+)/favorite$', views.favorite, name='favorite'),
+]
